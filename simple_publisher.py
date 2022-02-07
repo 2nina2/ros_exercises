b5ce657d0f58f23a0185e98ce80fdba8f5afda9d
Nina Gerszberg <ninager@mit.edu>
2022-02-07 21:20:33 +0000

    wrote random number generator publisher node

--- simple_publisher.py
+++ simple_publisher.py
@@ -1,10 +1,18 @@
-import rospy
-from std_msgs.msg import String
+#!/usr/bin/env python
+import rospy, random
+from std_msgs.msg import Float32
+from random import randint
 
 if __name__ == '__main__':
   try:
+    rospy.init_node("simple_publisher")
     rate = rospy.Rate(20)
-    pub = ("my_random_float", Float32)
-    rospy.init_node("simple_publisher",anonymous=True))
+    pub = rospy.Publisher("my_random_float", Float32, queue_size = 10)
     while not rospy.is_shutdown():
-      num = 
+      num = randint(0,10)
+      rospy.loginfo("The random number is: %s",num)
+      pub.publish(num)
+      rate.sleep()
+  except rospy.ROSInterruptException:
+    pass
+
